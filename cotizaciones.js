e7df549470268371e6a6f4d39ec8495e30021548
luvaras6 <lucia.varas6@gmail.com>
2022-06-14 17:42:00 -0300

    Agregado de cotizaciones en JavaScript. Falta estilos.

--- cotizaciones.js
+++ cotizaciones.js
@@ -1,19 +1,27 @@
-let cardCotizaciones = document.getElementsByClassName("cotizaciones");
+let cardCotizaciones = document.querySelector(".cotizaciones");
 
-let cotizaciones = [];
 const regex = /(Dolar)\s[B]/gm;
 const regex2 = /(Dolar)\s[OCt]/gm;
 
+
 fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales')
-    .then(data => data.json())
-    .then(data => data.forEach(element => {
-        if (regex.test(element.casa.nombre) || regex2.test(element.casa.nombre)) {
-            const usd = {
-                nombre: element.casa.nombre,
-                compra: element.casa.compra,
-                venta: element.casa.venta   
-            };
-            cotizaciones.push(usd);
-        }}));
-                
-console.log(cotizaciones);
+.then(data => data.json())
+.then(data => data.forEach(element => {
+    if (regex.test(element.casa.nombre) || regex2.test(element.casa.nombre)) {
+        const usd = {
+            nombre: element.casa.nombre,
+            compra: element.casa.compra,
+            venta: element.casa.venta   
+        };
+
+        const Items = (usd) => {
+            return `<div class="dolar-div">
+                        <p class="dolar-nombre">${usd.nombre}</p>
+                        <p class="dolar-compra">Compra $${usd.compra}</p>
+                        <p class="dolar-venta">Venta $${usd.venta}</p>
+                    </div>`
+        }
+
+        cardCotizaciones.innerHTML += Items(usd);
+    }
+}))
