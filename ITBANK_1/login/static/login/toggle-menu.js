2578fea2ee2a911401da1fdf109eee31acbbb050
NanoB0tt <marianofassikoropecki@gmail.com>
2022-08-14 04:13:48 -0300

    eliminado mas pycache, arreglado responsive (maso)

--- ITBANK_1/login/static/login/toggle-menu.js
+++ ITBANK_1/login/static/login/toggle-menu.js
@@ -1,30 +1,16 @@
 const button = document.querySelector('.hamburger-menu')
 const leftBar = document.querySelector('.left-bar')
-const content = document.querySelector('.section-grid')
-const cotiza = document.querySelector('.cotizaciones')
+const body = document.querySelector('body')
 
 
 button.addEventListener('click', () => {
-	if (leftBar.classList.contains('hidden')) {
-		leftBar.classList.remove('hidden');
+    if (leftBar.classList.contains('hidden')) {
+        leftBar.classList.remove('hidden')
+        body.style.setProperty('overflow-y', 'hidden')
 
-		/*remove section grid*/
-		content.classList.add('hidden');
-		content.classList.remove('section-grid')
-
-		/*remove cotizaciones*/
-		cotiza.classList.add('hidden');
-		cotiza.classList.remove('cotizaciones')
-
-	} else {
-		leftBar.classList.add('hidden');
-
-		/*add section grid*/
-		content.classList.remove('hidden');
-		content.classList.add('section-grid')
-
-		/*add cotizaciones*/
-		cotiza.classList.remove('hidden')
-		cotiza.classList.add('cotizaciones');
-	}
+    } else {
+        leftBar.classList.add('hidden')
+        body.style.setProperty('overflow-y', 'auto')
+    }
 })
+
